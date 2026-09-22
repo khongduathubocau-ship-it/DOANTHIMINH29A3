d93f621cc92272268727caac334a9c525e9d33b4
khongduathubocau-ship-it <khongduathubocau@gmail.com>
2026-09-22 10:01:42 +0700

    Delete B15CH1.py

--- B15CH1.py
+++
@@ -1,6 +0,0 @@
-n = int(input("Nhập số nguyên dương n: "))
-tong_binh_phuong = 0
-for i in range(1, n + 1):
-    if i % 2 != 0: # Kiểm tra số lẻ
-        tong_binh_phuong = tong_binh_phuong + i * i
-print(f"Tổng bình phương các số lẻ từ 1 đến {n} là: {tong_binh_phuong}")
